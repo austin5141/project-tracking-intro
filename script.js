08dffa551b046742dd6ce360a6c1de69fcbdaf1f
Austin B <austin51915@gmail.com>
2024-08-26 13:23:02 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const hamburger = document.querySelector(".open")
+const x_button = document.querySelector(".close")
+const mobile_links = document.querySelector('.mobile_links')
+
+function openMenu() {
+    hamburger.classList.add('hidden')
+    x_button.classList.remove('hidden')
+    mobile_links.classList.remove('hidden')
+}
+
+function closeMenu() {
+    x_button.classList.add('hidden')
+    hamburger.classList.remove('hidden')
+    mobile_links.classList.add('hidden')
+}
